f09ef8107ec6ecc69e5dadc8b98493a65d412348
Mucsi Róbert <47422012+mucsirobert@users.noreply.github.com>
2020-10-29 12:53:40 +0100

    Complete
    Totally complete

--- main.cpp
+++ main.cpp
@@ -97,64 +97,7 @@ int main()
         std::cin >> str;
         std::cout << str << " " << str.getLength() << std::endl;
     }
-/*
-    const char * c = "IIIIIIIIIIIIIII";
-    MyString s{c};
-    //std::cin >> s;
-    MyString text = "text";
-    //MyString a,b,c;
-   // MyString a{s};
-    MyString copied{text};
-    MyString cica = "cica";
-    MyString cic2{cica};
-    MyString kutya = "kutya";
-    char x = 'x';
-    std::cout << " x create: \n";
-
-    kutya = kutya +x;
-    std::cout<<kutya <<std::endl;
-
-
-
-   // a = s;
-//    std::cout <<"mem: "<< *s.refcnt << " " << *a.refcnt;
-//    std::cout <<"mem: "<< s.str << " " << a.str;
-//    s += 'x';
-//    std::cout <<"mem: "<< s.str << " " << a.str;
-//    s.~MyString();
-
-   // a = s;
-
-    std::cout << "str: "<< s << " cnt: " << s.getCount() << " len: " << s.getLength() << std::endl;
-
-    std::cout << "str: "<< text << " cnt: " << text.getCount() << " len: " <<  text.getLength() << "\n = happen " << std::endl;
-
-    s = text;
-
-    std::cout << "text.str: "<< text << " cnt: " << text.getCount() << " len: " <<  text.getLength() << std::endl;
-    s[1] = 'O';
-    std::cout << "s.str: "<< s << " cnt: " << s.getCount() << " len: " << s.getLength() << std::endl;
-
-    std::cout << "text.str: "<< text << " cnt: " << text.getCount() << " len: " <<  text.getLength() << std::endl;*/
-  //  std::cout<<a<<*a.refcnt<<std::endl;
-
-//    const char *o = "poiuz";
-//    const char *i;
-//    i=o;
-//    std::cout<< o<<std::endl;
-//    std::cout<< i;
-
-    //std::cout << a <<std::endl;
 
     return 0;
 
-
-//    MyString a, b, c;
-//    a = b = c = "helló világ";
-
-//    MyString x = "hello";
-//    MyString y = x;
-//    y[0] = 'c';
-//    std::cout << x;     /* hello */
-//    std::cout << y;     /* cello */
 }
--- mystring.cpp
+++ mystring.cpp
@@ -2,41 +2,36 @@
 #include <iostream>
 #include <cstring>
 
-//talán jó
 MyString::MyString(const char *c){
-//    std::cout << "str pointer memory address" << &str << std::endl;
-    if (c) {
+    /*if (c) {
         str = new char[strlen(c) + 1];
         strcpy(str, c);
         refcnt = new int{1};
+    }*/
+    strlen(c) > 0 ? str = new char[strlen(c)+1] : str = new char[5];
+    strcpy(str, c);
+    if(str!=nullptr){
+        refcnt = new int {1};
     }
-
+    else
+        refcnt = nullptr;
 
 };
 
-//talán jó
 MyString::MyString(MyString const &source): str{source.str}, refcnt{source.refcnt}{
-  //  std::cout << "copy ctor\n";
     if(str){
         ++ *refcnt;
     }
 }
-// talán jó
-MyString::MyString(MyString && the_other){
-   // std::cout <<"move ctor\n";
-   /* if(str != nullptr)
-        delete[] str;*/
-   //std::cout <<"move ctor e\n" << the_other.getStr();
-    //str = new char[the_other.getLength()];
-    //strcpy(str,the_other.str);
-    //this->refcnt = the_other.refcnt;
+
+MyString::MyString(MyString && the_other) noexcept{
     str = the_other.str;
     refcnt = the_other.refcnt;
     the_other.str = nullptr;
     the_other.refcnt = nullptr;
 };
 
-void MyString::destruct(){
+void MyString::destruct() noexcept{
     if(str != nullptr){
         -- *refcnt;
         if(*refcnt == 0) {
@@ -46,33 +41,24 @@ void MyString::destruct(){
     }
 }
 
-//elvileg jó
 MyString::~MyString(){
     destruct();
 }
 
 MyString& MyString::operator= (MyString const &rhs){
-    std::cout << "const value giver\n";
-    if (this != &rhs) { // self-assignment check expected
+    if (this != &rhs) {
         destruct();
-                      // destroy storage in this
-
-                     // preserve invariants in case next line throws
-
-        this->str = new char[strlen(rhs.str)]; // create storage in this
+        //this->str = new char[strlen(rhs.str)];
         refcnt = rhs.refcnt;
         ++*rhs.refcnt;
         this->str = rhs.str;
-        //strcpy(this->str,rhs.str);
     }
     return *this;
 }
 
 
-MyString& MyString::operator= (MyString && the_other){
-    //std::cout <<"move operator\n";
+MyString& MyString::operator= (MyString && the_other) noexcept{
     if(&the_other != this  ){
-           // std::cout <<"a 1 \n";
         this->destruct();
         refcnt = the_other.refcnt;
         str = the_other.str;
@@ -82,22 +68,11 @@ MyString& MyString::operator= (MyString && the_other){
     return *this;
 };
 
-//talán jó
-MyString operator+ (MyString lhs, const MyString& rhs){/*
-    char cat[lhs.getLength() + rhs.getLength()+1];
-    strcpy(cat,lhs.getStr());
-    strcat(cat,rhs.getStr());
-    cat[lhs.getLength() + rhs.getLength()] = '\0';*/
-//    MyString my{cat};
-    //std::cout << "+ operator new length: " << cat << " " << my.getLength();
+MyString operator+ (MyString lhs, const MyString& rhs){
     return lhs += rhs;
 };
 
 MyString operator+ (MyString lhs, const char rhs){
-    /*char cat[lhs.getLength()+2];
-    strcpy(cat,lhs.getStr());
-    cat[lhs.getLength()] = rhs;
-    cat[lhs.getLength()+1] = '\0';*/
     return lhs += rhs;
 }
 
@@ -105,67 +80,27 @@ MyString& MyString::operator+= (const MyString rhs){
     if(rhs.getLength() == 0){
         return *this;
     }
-        char *new_str = new char(strlen(str)+rhs.getLength()+1);
-        strcpy(new_str, this->getStr());
-        strcat(new_str, rhs.getStr());
-        new_str[strlen(str)+rhs.getLength()] = '\0';
-        destruct();
-        this->str = new_str;
-        this->refcnt = new int{1};
-
-    /*
-    if (1 < this->getCount()){
-        char *new_str = new char(strlen(str)+rhs.getLength()+1);
-        strcpy(new_str, this->getStr());
-        strcat(new_str, rhs.getStr());
-        new_str[strlen(str)+rhs.getLength()] = '\0';
-//        std::cout << toCopyOnWrite << "  :asdfg\n";
-        -- *this->refcnt;
-        this->str = new_str;
-        this->refcnt = new int(1);
-    }
-    else{
-        char *new_str = new char(strlen(str)+rhs.getLength()+1);
-        strcpy(new_str, this->getStr());
-        strcat(new_str, rhs.getStr());
-        new_str[strlen(str)+rhs.getLength()] = '\0';
-        delete[] str;
-        this->str = new_str;
-    }*/
+    char *new_str = new char[strlen(str)+rhs.getLength()+1];
+    strcpy(new_str, this->getStr());
+    strcat(new_str, rhs.getStr());
+    new_str[strlen(str)+rhs.getLength()] = '\0';
+    destruct();
+    this->str = new_str;
+    this->refcnt = new int{1};
     return *this;
 }
 
 MyString& MyString::operator+= (const char rhs){
-    char* new_str = new char(strlen(str)+2);
+    char* new_str = new char[strlen(str)+2];
     strcpy(new_str, this->getStr());
     new_str[this->getLength()] = rhs;
     new_str[this->getLength()+1] = '\0';
     destruct();
     this->str = new_str;
-    this->refcnt = new int {1};/*
-    if(1 <this->getCount()){
-        char* new_str = new char(strlen(str)+2);
-            strcpy(new_str, this->getStr());
-        new_str[this->getLength()] = rhs;
-        new_str[this->getLength()+1] = '\0';
-        -- *this->refcnt;
-        this->str = new_str;
-        this->refcnt = new int(1);
-    }
-    else{
-        char *new_str = new char(strlen(str)+2);
-        strcpy(new_str, this->getStr());
-        new_str[this->getLength()] = rhs;
-        new_str[this->getLength()+1] = '\0';
-        delete[] str;
-        this->str = new_str;
-    }*/
+    this->refcnt = new int {1};
     return *this;
 };
 
-
-
-//talán jó
 std::ostream& operator<<(std::ostream& os, MyString& ms){
     os << ms.getStr();
     return os;
@@ -175,83 +110,53 @@ std::ostream& operator<<(std::ostream& os, MyString const& ms){
     os << ms.getStr();
     return os;
 }
-/*
-void MyString::set_str(char *c){
-        this->destruct();
-        str = new char(strlen(c)+1);
-        strcpy(str, c);
-        if(str){
-            refcnt = new int(1);
-        }
-        else
-            refcnt = nullptr;
-    }*/
 
 std::istream& operator>>(std::istream& is, MyString &ms){
-    /*char *c;
-    is >> *c;
-    ms.set_str(c);
-    is.get();*/
-
-
-    int n = 0;
+    int th = 0;
     char c;
-    char *a = new char[11];
+    char *input = new char[11];
     int allocated = 11;
     while( is.get(c) ){
         if( '\n' == c  ||  '\r' == c )
             break;
+        input[th] = c;
 
-        a[n] = c;
-
-        if ( n >= allocated-1 ){
+        if ( th >= allocated-1 ){
             allocated *= 2;
-            char *b = new char[allocated];
-//            for ( int i = 0; i <= n; ++i )
-//                b[i] = a[i];
-            strcpy(b,a);
-            delete[] a;
-            a = b;
+            char *new_place = new char[allocated];
+            strcpy(new_place,input);
+            delete[]new_place;
         }
-
-        ++n;
+        ++th;
     }
-    a[n] = '\0';
-
-    ms = MyString{a};
-    delete[] a;
-//    ms.set_str(a);
-
+    input[th] = '\0';
+    ms = MyString{input};
+    delete[] input;
     return is;
 }
 
-int MyString::getCount() const {return *refcnt;}
+int MyString::getCount() const noexcept {return *refcnt;}
 
-int MyString::getLength() const {return strlen(str);}
-
-
+int MyString::getLength() const noexcept {return strlen(str);}
 
 char& MyString::operator[](int i){
     if(i >= this->getLength() || i < 0){
         throw std::out_of_range("Index out of bound");
     }
     if(1 < *this->refcnt){
-        //std::cout <<"inside: copyOnWriteCall\n";
-        char *toCopyOnWrite = new char(strlen(this->str));
-
+        char *toCopyOnWrite = new char[strlen(this->str)];
         strcpy(toCopyOnWrite, this->str);
-//        std::cout << toCopyOnWrite << "  :asdfg\n";
         -- *this->refcnt;
         this->str = toCopyOnWrite;
-        this->refcnt = new int(1);
+        this->refcnt = new int{1};
     }
-
-        //std::cout <<"copyOnWriteCall\n";
-//        std::cout << "nope:\n";
     return str[i];
 }
 
 char const& MyString::operator[] (int i) const {
+    if(i >= this->getLength() || i < 0){
+        throw std::out_of_range("Index out of bound");
+    }
     return str[i];
 }
 
